c054b4fcb10e64dfa6e648588081c73885d0440d
Ziven Bello <zivbello@gmail.com>
2026-04-07 20:32:20 -0400

    Update HashPWStorage.py

--- HashPWStorage.py
+++ HashPWStorage.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+# Profile table
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f"Name : {self.first_name}, Password : {self.password}"
 
 @app.route("/")
 def hello_world():
