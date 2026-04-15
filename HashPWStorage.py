0d38d5ae2de5c4b6467345c0804396ff71b02c8e
Ziven Bello <zivbello@gmail.com>
2026-04-14 20:32:24 -0400

    Edited main .py file and added html files

--- HashPWStorage.py
+++ HashPWStorage.py
@@ -1,29 +1,62 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, request, redirect
+from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+import hashlib
 
-# Configure SQLite database
+app = Flask(__name__)
+app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
 
 db = SQLAlchemy(app)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
-
-# Profile table
+# Model
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(20), unique=False, nullable=False)
-    last_name = db.Column(db.String(20), unique=False, nullable=False)
-    password = db.Column(db.String(20), unique=False, nullable=False)
-    
-    def __repr__(self):
-        return f"Name : {self.first_name}, Password : {self.password}"
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    pwh = db.Column(db.String(20), nullable=False)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Name : {self.name}, Password Hash: {self.pwh}"
+
+# function to render index page
+@app.route('/')
+def index():
+    profiles = Profile.query.all()
+    return render_template('index.html', profiles=profiles)
+
+@app.route('/add_data')
+def add_data():
+    return render_template('add_profile.html')
+
+# function to add profiles
+@app.route('/add', methods=["POST"])
+def profile():
+    name = request.form.get("name")
+    pw = request.form.get("password")
+    pwh = hashlib.sha256()
+    pwh.update(pw)
+    pwh.hexdigest
+
+    if name != '' and pw != '':
+        p = Profile(name=name, pwh=pwh)
+        db.session.add(p)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route('/delete/<int:id>')
+def erase(id): 
+    data = Profile.query.get(id)
+    db.session.delete(data)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    with app.app_context():  # Needed for DB operations outside a request
+        db.create_all()      # Creates the database and tables
+    app.run(debug=True)
