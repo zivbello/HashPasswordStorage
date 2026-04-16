3e3574e41ec1df1da4fbdd8f344f7102c17629af
Ziven Bello <zivbello@gmail.com>
2026-04-15 20:06:43 -0400

    Added login page and function

--- HashPWStorage.py
+++ HashPWStorage.py
@@ -17,13 +17,13 @@ db = SQLAlchemy(app)
 # Model
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=False, nullable=False)
+    username = db.Column(db.String(20), unique=False, nullable=False)
     pwh = db.Column(db.String(20), nullable=False)
 
     # repr method represents how one object of this datatable
     # will look like
     def __repr__(self):
-        return f"Name : {self.name}, Password Hash: {self.pwh}"
+        return f"Name : {self.username}, Password Hash: {self.pwh}"
 
 # function to render index page
 @app.route('/')
@@ -31,28 +31,47 @@ def index():
     profiles = Profile.query.all()
     return render_template('index.html', profiles=profiles)
 
+# function to render add profile page
 @app.route('/add_data')
 def add_data():
     return render_template('add_profile.html')
 
+# function to render login page and check passwords
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    msg = ''
+    if request.method == "POST" and 'username' in request.form and 'password' in request.form:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        pwh = hashlib.sha256()
+        pwh.update(password.encode("utf-8"))
+        pwh_hash = pwh.hexdigest()
+        user = db.one_or_404(db.select(Profile).filter_by(username=username))
+        if user.pwh == pwh_hash:
+            return redirect('/') # TODO: Create a page to navigate to after logging in
+        else:
+            msg = "Login unsuccessful. Incorrect username/password!"
+    return render_template('login.html', msg=msg)
+
 # function to add profiles
 @app.route('/add', methods=["POST"])
 def profile():
-    name = request.form.get("name")
+    username = request.form.get("username")
     pw = request.form.get("password")
     pwh = hashlib.sha256()
     pwh.update(pw.encode("utf-8"))
     pwh_hash = pwh.hexdigest()
     pwh_str = f"{pwh_hash}"
 
-    if name != '' and pw != '':
-        p = Profile(name=name, pwh=pwh_str)
+    if username != '' and pw != '':
+        p = Profile(username=username, pwh=pwh_str)
         db.session.add(p)
         db.session.commit()
         return redirect('/')
     else:
         return redirect('/')
 
+# function to delete table entries
 @app.route('/delete/<int:id>')
 def erase(id): 
     data = Profile.query.get(id)
